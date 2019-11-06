86a2284e8beff9b1f07272a203c86a72749516ec
garybot <argot.pryer@gmail.com>
2019-11-06 14:04:04 -0800

    minor refactor and removal of logs.

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,17 +1,11 @@
 import { useEffect } from 'react';
 import { useLocalStorage } from './useLocalStorage.js';
-// import classNames from 'classnames';
 
 export const useDarkMode = () => {
   const [darkMode, setDarkMode] = useLocalStorage('isModeDark');
   useEffect(() => {
     const body = document.getElementsByTagName('body')[0];
-    console.log(body);
-    if (darkMode) {
-      body.className = "dark-mode";
-    } else {
-      body.className = "";
-    }
+    darkMode ? body.classList.add("dark-mode") : body.classList.remove("dark-mode");
   }, [darkMode])
   return [darkMode, setDarkMode]
 }
